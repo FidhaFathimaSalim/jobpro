291421561ef81ed659a6b216aa121872a9f24bec
Fidha Fathima Salim <fidhafathimasalim73@nirmalacollege.ac.in>
2025-02-13 04:54:03 +0530

    changed

--- hirehub/jobportal/urls.py
+++ hirehub/jobportal/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.work,name='work'),
     path('recommend_jobs/', views.recommend_jobs, name='recommend_jobs'),
     path('hirebot/', views.hirebot, name='hirebot'),
-
+    path('cv',views. cv_create, name='cv_create'),
+    path('<int:profile_id>/', views.cv_download, name='cv_download'),
+    path('view/<int:profile_id>/', views.cv_view, name='cv_view'),
+    path('delete/<int:profile_id>/', views.cv_delete, name='cv_delete'),
+    path('update/<int:profile_id>/', views.cv_update, name='cv_update'),
+    path('list', views.list_view, name='list_view'),
 ]
